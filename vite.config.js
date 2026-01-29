d9277c60a0da6222813b7b6608c4886ba8a33cd7
HBull5 <harcourtbullv@gmail.com>
2026-01-29 18:28:03 -0500

    Repo name update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // CHANGE this to your repo name:
-const repoName = "JessicaWebsite";
+const repoName = "JMBullArt";
 
 export default defineConfig({
   plugins: [react()],
