073f8fed5b4a91cf954cf6cba643d9e8856bdf40
HBull5 <harcourtbullv@gmail.com>
2026-01-29 19:38:05 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// CHANGE this to your repo name:
-const repoName = "JMBullArt";
-
 export default defineConfig({
   plugins: [react()],
-  base: `/${repoName}/`,
-});
+  base: '/'
+}); 
